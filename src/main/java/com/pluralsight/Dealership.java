ca890259c63d8066f605aaa71b11a96100bd186b
birukbelaytafese <birukbelay175@gmail.com>
2026-05-08 12:15:27 -0500

    Dealership commit

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Dealership {
+
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+        return inventory;
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        inventory.add(vehicle);
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice() {
+        return null;
+    }
+}
