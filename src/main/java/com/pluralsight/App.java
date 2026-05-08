3adc9a89c806ff81f12c993ca08ba767818f797a
birukbelaytafese <birukbelay175@gmail.com>
2026-05-08 13:00:33 -0500

    app commit

---
+++ src/main/java/com/pluralsight/App.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class App {
+        public static void main(String[] args) {
+
+            UserInterface ui = new UserInterface();
+            ui.display();
+
+        }
+    }
